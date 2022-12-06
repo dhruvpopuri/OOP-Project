2ed8f86629f7b9c8bf6e48789610a28a3aa68521
yashksk <yash2830kaushik@gmail.com>
2022-12-06 15:11:21 +0530

    add card screen

--- src/flashcards/FlashCardBuilder.java
+++ src/flashcards/FlashCardBuilder.java
@@ -34,21 +34,28 @@ public class FlashCardBuilder {
 		answer.setFont(getFont);
 		
 		JScrollPane scrollpane = new JScrollPane(answer);
+		scrollpane.setBounds(11, 208, 363, 162);
 		scrollpane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		scrollpane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		
 		JScrollPane scrollpane_question = new JScrollPane(question);
+		scrollpane_question.setBounds(11, 23, 363, 162);
 		scrollpane_question.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 
 		scrollpane_question.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		
 		JButton nextButton = new JButton("Next Card");
+		nextButton.setBounds(102, 410, 77, 21);
 		JButton homeButton = new JButton("Home");
+		homeButton.setBounds(204, 410, 59, 21);
 		
 		cardList = new ArrayList<Card>();
 		
 		JLabel qlabel = new JLabel("Question");
+		qlabel.setBounds(173, 5, 40, 13);
 		JLabel alabel = new JLabel("Answer");
+		alabel.setBounds(176, 190, 34, 13);
+		mainPanel.setLayout(null);
 		
 		
 		mainPanel.add(qlabel);
@@ -78,6 +85,14 @@ public class FlashCardBuilder {
 		
 		
 		frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(102, 379, 161, 21);
+		mainPanel.add(comboBox);
+		
+		JLabel lblNewLabel = new JLabel("Card Type");
+		lblNewLabel.setBounds(21, 383, 71, 13);
+		mainPanel.add(lblNewLabel);
 		frame.setSize(400, 500);
 		frame.setVisible(true);
 	}
